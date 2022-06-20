e6e2f20808ea96f5678f574b53736f61524f8c23
Sagayaraj David <54359431+sagayd@users.noreply.github.com>
2022-06-20 18:22:46 +0530

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -1,1 +1,7 @@
-packer build jar_deploy.json
+sname=$(basename $BASH_SOURCE)
+sdir=$(cd `dirname $BASH_SOURCE` && pwd)
+
+PACKER_LOG=1
+PACKER_LOG_PATH=$sdir/packerlog.txt
+
+packer build $sdir/jar_deploy.json
